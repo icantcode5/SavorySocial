d1b0a3a89d838f106adcbcfdbc3f68beaeddda18
Carlos Sosa <carlossosa54321@gmail.com>
2023-08-27 19:19:50 -0700

    set up server and more dev environment config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
-const addTwoNums = (a: number, b: number) => {
-	return a + b
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import path from "path"
+import dotenv from "dotenv"
+
+const PORT = 3000
+dotenv.config({ path: "./config/.env" })
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json)
+
+//Serve Frontend files when running in production
+if (process.env.NODE_ENV === "production") {
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "./Frontend/dist/index.html"))
+	})
 }
 
-addTwoNums(5, 5)
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
